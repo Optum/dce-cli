49dba2f1aa3e987517d86dccd4b49c2b074409a2
Joshua Marsh <joshmrsh@amazon.com>
2019-10-10 10:04:06 -0500

    WIP: Deploy command successfully deployed some terraform infra

--- cmd/system.go
+++ cmd/system.go
@@ -2,15 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
+	terraform "github.com/hashicorp/terraform/command"
+	"github.com/mitchellh/cli"
 	"github.com/spf13/cobra"
 )
 
-var dceVersion string
-
 func init() {
 
-	systemDeployCmd.Flags().StringVarP(&dceVersion, "dce-version", "s", "", "DCE version to deploy (Defaults to latest)")
 	systemCmd.AddCommand(systemDeployCmd)
 
 	systemLogsCmd.AddCommand(systemLogsAccountsCmd)
@@ -23,22 +25,12 @@ func init() {
 	systemUsersCmd.AddCommand(systemUsersRemoveCmd)
 	systemCmd.AddCommand(systemUsersCmd)
 
-	systemCmd.AddCommand(systemInitCmd)
-
 	RootCmd.AddCommand(systemCmd)
 }
 
 var systemCmd = &cobra.Command{
 	Use:   "system",
 	Short: "Deploy and configure the DCE system",
-}
-
-var systemInitCmd = &cobra.Command{
-	Use:   "init",
-	Short: "Initialize a DCE configuration file at the specified location (Defaults to ~/.dce.yaml)",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("Init command")
-	},
 }
 
 /*
@@ -48,6 +40,154 @@ Deploy Namespace
 var systemDeployCmd = &cobra.Command{
 	Use:   "deploy",
 	Short: "Deploy the DCE system",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Read template from internal and write into a /tmp file, init there
+		tfBackendTemplate :=
+			`provider "aws" {
+region = "us-east-1"
+}
+
+variable "global_tags" {
+description = "The tags to apply to all resources that support tags"
+type        = map(string)
+
+default = {
+	Terraform = "True"
+	AppName   = "AWS Redbox Management"
+	Source    = "github.com/Optum/Redbox//modules"
+	Contact   = "fake_email@domain.com"
+}
+}
+
+variable "namespace" {
+type = string
+}
+
+data "aws_caller_identity" "current" {}
+
+# Configure an S3 Bucket to hold artifacts
+# (eg. application code deployments, etc.)
+resource "aws_s3_bucket" "local_tfstate" {
+bucket = "${data.aws_caller_identity.current.account_id}-local-tfstate-${var.namespace}"
+
+# Allow S3 access logs to be written to this bucket
+acl = "log-delivery-write"
+
+# Allow Terraform to destroy the bucket
+# (so ephemeral PR environments can be torn down)
+force_destroy = true
+
+# Encrypt objects by default
+server_side_encryption_configuration {
+	rule {
+	apply_server_side_encryption_by_default {
+		sse_algorithm = "AES256"
+	}
+	}
+}
+
+versioning {
+	enabled = true
+}
+
+# Send S3 access logs for this bucket to itself
+logging {
+	target_bucket = "${data.aws_caller_identity.current.account_id}-local-tfstate-${var.namespace}"
+	target_prefix = "logs/"
+}
+
+tags = var.global_tags
+}
+
+# Enforce SSL only access to the bucket
+resource "aws_s3_bucket_policy" "reset_codepipeline_source_ssl_policy" {
+bucket = aws_s3_bucket.local_tfstate.id
+
+policy = <<POLICY
+{
+	"Version": "2012-10-17",
+	"Statement": [
+	{
+		"Sid": "DenyInsecureCommunications",
+		"Effect": "Deny",
+		"Principal": "*",
+		"Action": "s3:*",
+		"Resource": "${aws_s3_bucket.local_tfstate.arn}/*",
+		"Condition": {
+			"Bool": {
+				"aws:SecureTransport": "false"
+			}
+		}
+	}
+	]
+}
+POLICY
+
+}
+
+resource "aws_dynamodb_table" "local_terraform_state_lock" {
+name           = "Terraform-State-Backend-${var.namespace}"
+read_capacity  = 1
+write_capacity = 1
+hash_key       = "LockID"
+
+attribute {
+	name = "LockID"
+	type = "S"
+}
+}
+
+output "bucket" {
+value = aws_s3_bucket.local_tfstate.bucket
+}
+`
+		tempDir, err := ioutil.TempDir("", "dce-init-")
+		currentDir, err := os.Getwd()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		os.Chdir(tempDir)
+		defer os.Chdir(currentDir)
+		defer os.RemoveAll(tempDir)
+
+		if err != nil {
+			log.Fatalf("Error: ", err)
+		}
+
+		fileName := tempDir + "/" + "init.tf"
+		err = ioutil.WriteFile(fileName, []byte(tfBackendTemplate), 0644)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+
+		log.Println("Created temporary terraform working directory at: ", tempDir)
+
+		ui := &cli.BasicUi{
+			Reader:      os.Stdin,
+			Writer:      os.Stdout,
+			ErrorWriter: os.Stderr,
+		}
+
+		tfInit := &terraform.InitCommand{
+			Meta: terraform.Meta{
+				Ui: ui,
+			},
+		}
+		// initReturn := tfInit.Run([]string{"--plugin-dir", tempDir})
+		initReturn := tfInit.Run([]string{})
+		log.Println(initReturn)
+
+		tfApply := &terraform.ApplyCommand{
+			Meta: terraform.Meta{
+				Ui: ui,
+			},
+		}
+		applyReturn := tfApply.Run([]string{})
+		log.Println(applyReturn)
+
+		// EASY TEMPORARY OPTION FOR LAMBDA ARTIFACT DEPLOYMENT:
+		//   Use flag to set path to local DCE repo. Build all artifacts there and deploy them.
+	},
 }
 
 /*
--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.13
 
 require (
 	github.com/aws/aws-sdk-go v1.25.6
+	github.com/hashicorp/terraform v0.12.10
 	github.com/manifoldco/promptui v0.3.2
+	github.com/mitchellh/cli v1.0.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.4.0
-	github.com/stretchr/testify v1.2.2
+	github.com/stretchr/testify v1.3.0
 	gopkg.in/yaml.v2 v2.2.2
 )
