15509bde0cd2230a9a8597159359c0ba1b27a38b
Joshua Marsh <joshmrsh@amazon.com>
2019-12-30 13:34:56 -0600

    WIP: replacing graphql api with latest download url

--- internal/util/github.go
+++ internal/util/github.go
@@ -1,16 +1,12 @@
 package util
 
 import (
-	"context"
 	"io"
 	"net/http"
 	"os"
 
 	"github.com/Optum/dce-cli/configs"
-	"github.com/Optum/dce-cli/internal/constants"
 	observ "github.com/Optum/dce-cli/internal/observation"
-	"github.com/shurcooL/githubv4"
-	"golang.org/x/oauth2"
 )
 
 type GithubUtil struct {
@@ -19,39 +15,47 @@ type GithubUtil struct {
 }
 
 func (u *GithubUtil) DownloadGithubReleaseAsset(assetName string) {
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *u.Config.GithubToken},
-	)
-	oauthHTTPClient := oauth2.NewClient(context.Background(), src)
+	//src := oauth2.StaticTokenSource(
+	//	&oauth2.Token{AccessToken: *u.Config.GithubToken},
+	//)
+	//oauthHTTPClient := oauth2.NewClient(context.Background(), src)
+	//
+	//variables := map[string]interface{}{
+	//	"assetName": githubv4.String(assetName),
+	//	"repoName":  githubv4.String(constants.RepoName),
+	//	"repoOwner": githubv4.String(constants.RepoOwner),
+	//}
+	//
+	//var query struct {
+	//	Repository struct {
+	//		Releases struct {
+	//			Nodes []struct {
+	//				ReleaseAssets struct {
+	//					Nodes []struct {
+	//						URL string
+	//					}
+	//				} `graphql:"releaseAssets(last: 1, name: $assetName)"`
+	//			}
+	//		} `graphql:"releases(last: 1)"`
+	//	} `graphql:"repository(owner: $repoOwner, name: $repoName)"`
+	//}
+	//
+	//githubClient := githubv4.NewClient(oauthHTTPClient)
+	//err := githubClient.Query(context.Background(), &query, variables)
+	//if err != nil {
+	//	log.Fatalf("error: %v", err)
+	//}
+	//log.Debug("Github Query Response:", query.Repository.Releases.Nodes[0].ReleaseAssets.Nodes[0].URL)
 
-	variables := map[string]interface{}{
-		"assetName": githubv4.String(assetName),
-		"repoName":  githubv4.String(constants.RepoName),
-		"repoOwner": githubv4.String(constants.RepoOwner),
-	}
 
-	var query struct {
-		Repository struct {
-			Releases struct {
-				Nodes []struct {
-					ReleaseAssets struct {
-						Nodes []struct {
-							URL string
-						}
-					} `graphql:"releaseAssets(last: 1, name: $assetName)"`
-				}
-			} `graphql:"releases(last: 1)"`
-		} `graphql:"repository(owner: $repoOwner, name: $repoName)"`
-	}
 
-	githubClient := githubv4.NewClient(oauthHTTPClient)
-	err := githubClient.Query(context.Background(), &query, variables)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	log.Debug("Github Query Response:", query.Repository.Releases.Nodes[0].ReleaseAssets.Nodes[0].URL)
+	//req, err := http.NewRequest("GET", query.Repository.Releases.Nodes[0].ReleaseAssets.Nodes[0].URL, nil)
+	req, err := http.NewRequest("GET", "https://github.com/Optum/dce/releases/latest/download/" + assetName, nil)
 
-	req, err := http.NewRequest("GET", query.Repository.Releases.Nodes[0].ReleaseAssets.Nodes[0].URL, nil)
+
+
+
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Fatalf("error: %v", err)
