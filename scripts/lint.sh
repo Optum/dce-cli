8602630700ca976ffebf856c7b06282182c6a2d4
Joshua Marsh <joshmrsh@amazon.com>
2020-02-19 13:04:20 -0600

    removed print statements from script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 echo -n "Formatting golang code... "
 gofmtout=$(go fmt ./...)
 if [ "$gofmtout" ]; then
-  echo $(diff <(gofmt cmd/leases.go) cmd/leases.go)
-  echo $(diff <(gofmt pkg/service/leases.go) pkg/service/leases.go)
   printf "\n\n"
   echo "Files with formatting errors:"
   echo "${gofmtout}"
