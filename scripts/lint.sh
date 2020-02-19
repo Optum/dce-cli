c4a2f62039c6e87f9324e5b2bad8db72a5b2a431
Joshua Marsh <joshmrsh@amazon.com>
2020-02-19 13:20:34 -0600

    removing unneeded bash

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -12,30 +12,10 @@ fi
 echo "done."
 
 echo -n "Linting golang code... "
-# TODO: Make sure golangci-lint is installed and ready to be run
-#GOLANG_LINT_CMD=golangci-lint
-#
-#if [ ! "$(command -v ${GOLANG_LINT_CMD})" ]; then
-#  echo -n "installing ${GOLANG_LINT_CMD}... "
-#  go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
-#fi
-
 golangci-lint run
 echo "done."
 
 
 echo -n "Scanning for securirty issues... "
-#GOSEC_CMD=gosec
-#
-#if [ ! "$(command -v ${GOSEC_CMD})" ]; then
-#  echo -n "installing ${GOSEC_CMD}... "
-#  go get -u github.com/securego/gosec/cmd/gosec
-#fi
-
 gosec ./...
 echo "done."
-
-#GOSEC_VERSION=v2.2.0
-#curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b . $GOSEC_VERSION
-#./gosec ./...
-#rm gosec
