79795e6bd43ca9c09ab79e7cb939bff9e33be588
Joshua Marsh <joshmrsh@amazon.com>
2020-04-21 14:01:08 -0500

    refactored test to table driven format

--- tests/integration/leases_test.go
+++ tests/integration/leases_test.go
@@ -14,134 +14,140 @@ import (
 	"time"
 )
 
+type cliInputs []string
+
+type cliOutputs struct {
+	httpRequest interface{}
+	printedJson map[string]interface{}
+	printedString string
+}
+
+
 func TestLeasesCreate(t *testing.T) {
 
-	t.Run("with --expires-on flag", func(t *testing.T) {
-		leasesCreateTest(t, &leaseCreateTestCase{
-			commandArgs: []string{
+	tests := []struct {
+		name string
+		cliInputs
+		cliOutputs
+		// AccountID to return from the `POST /leases` endpoint
+		mockAccountID string
+	}{
+		{
+			"with --expires-on flag",
+			[]string{
 				"leases", "create",
 				"-p", "test-user",
 				"-b", "100", "-c", "USD",
 				"-e", "test@example.com",
 				"--expires-on", "1h",
 			},
-			expectedCreateLeaseRequest: operations.PostLeasesBody{
-				BudgetAmount:             aws.Float64(100),
-				BudgetCurrency:           aws.String("USD"),
-				BudgetNotificationEmails: []string{"test@example.com"},
-				ExpiresOn:                float64(time.Now().Add(time.Hour).Unix()),
-				PrincipalID:              aws.String("test-user"),
-			},
-			mockAccountID: "123456789012",
-			expectedJSONOutput: map[string]interface{}{
-				"accountId":                "123456789012",
-				"budgetAmount":             float64(100),
-				"budgetCurrency":           "USD",
-				"budgetNotificationEmails": []interface{}{"test@example.com"},
-				"principalId":              "test-user",
-				"expiresOn":                time.Now().Add(time.Hour).Unix(),
-			},
-		})
-	})
-
-}
-
-type leaseCreateTestCase struct {
-	// Args to pass to the dce-cli
-	commandArgs []string
-
-	// Request body we expect to bbe sent to the `POST /leases` endpoint
-	expectedCreateLeaseRequest operations.PostLeasesBody
-
-	// AccountID to return from the `POST /leases` endpoint
-	mockAccountID string
-
-	// JSON we expect the CLI operation to output to stdout
-	expectedJSONOutput map[string]interface{}
-}
-
-func leasesCreateTest(t *testing.T, testCase *leaseCreateTestCase) {
-	cli := NewCLITest(t)
-
-	// Mock the DCE API
-	api := &mocks.APIer{}
-
-	// Should send a `POST /leases` request
-	expectedLease := testCase.expectedCreateLeaseRequest
-	api.On("PostLeases",
-		mock.MatchedBy(func(params *operations.PostLeasesParams) bool {
-			// Check that ExpiresOn is within ~5s of our expectation
-			// to account for actual time passing
-			assert.InDelta(t, expectedLease.ExpiresOn, params.Lease.ExpiresOn, 5)
-
-			// Pull out ExpiresOn field,
-			// so we can assert.Equals on the rest of the struct
-			expectedLeaseCopy := expectedLease
-			expectedLeaseCopy.ExpiresOn = 0
-			actualLeaseCopy := params.Lease
-			actualLeaseCopy.ExpiresOn = 0
-
-			// Check the rest of the lease object
-			assert.Equal(t, expectedLeaseCopy, actualLeaseCopy)
-			return true
-		}), nil).
-		Return(func(params *operations.PostLeasesParams, wt runtime.ClientAuthInfoWriter) *operations.PostLeasesCreated {
-			// Return a Lease, which looks like the requested lease
-			// (but with an account ID)
-			return &operations.PostLeasesCreated{
-				Payload: &operations.PostLeasesCreatedBody{
-					AccountID:                testCase.mockAccountID,
-					BudgetAmount:             *params.Lease.BudgetAmount,
-					BudgetCurrency:           *params.Lease.BudgetCurrency,
-					BudgetNotificationEmails: params.Lease.BudgetNotificationEmails,
-					ExpiresOn:                params.Lease.ExpiresOn,
-					PrincipalID:              *params.Lease.PrincipalID,
+			cliOutputs{
+				httpRequest: operations.PostLeasesBody{
+					BudgetAmount:             aws.Float64(100),
+					BudgetCurrency:           aws.String("USD"),
+					BudgetNotificationEmails: []string{"test@example.com"},
+					ExpiresOn:                float64(time.Now().Add(time.Hour).Unix()),
+					PrincipalID:              aws.String("test-user"),
 				},
-			}
-		}, nil)
+				printedJson:   map[string]interface{}{
+					"accountId":                "123456789012",
+					"budgetAmount":             float64(100),
+					"budgetCurrency":           "USD",
+					"budgetNotificationEmails": []interface{}{"test@example.com"},
+					"principalId":              "test-user",
+					"expiresOn":                time.Now().Add(time.Hour).Unix(),
+				},
+			},
+			"123456789012",
+		},
+	}
 
-	// Mock the output writer
-	out := &mocks.OutputWriter{}
-	out.On("Write", mock.MatchedBy(func(out []byte) bool {
-		var actualJSONOutput map[string]interface{}
-		err := json.Unmarshal(out, &actualJSONOutput)
-		assert.Nil(t, err, "output should be valid JSON")
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
 
-		// Check that ExpiresOn is within ~5s of our expectation
-		// to account for actual time passing
-		_ = testCase.expectedJSONOutput
-		expectedExpiresOn := testCase.expectedJSONOutput["expiresOn"].(int64)
-		actualExpiresOn := actualJSONOutput["expiresOn"].(float64)
-		assert.InDelta(t,
-			expectedExpiresOn,
-			actualExpiresOn,
-			5,
-		)
+			cli := NewCLITest(t)
 
-		// Pull out the expiresOn field,
-		// so we can assert.Equals on the rest of JSON
-		actualJSONOutput["expiresOn"] = int64(0)
-		testCase.expectedJSONOutput["expiresOn"] = int64(0)
+			// Mock the DCE API
+			api := &mocks.APIer{}
 
-		assert.Equal(t, testCase.expectedJSONOutput, actualJSONOutput)
+			// Should send a `POST /leases` request
+			expectedLease := testCase.cliOutputs.httpRequest.(operations.PostLeasesBody)
+			api.On("PostLeases",
+				mock.MatchedBy(func(params *operations.PostLeasesParams) bool {
+					// Check that ExpiresOn is within ~5s of our expectation
+					// to account for actual time passing
+					assert.InDelta(t, expectedLease.ExpiresOn, params.Lease.ExpiresOn, 5)
 
-		return true
-	})).Return(0, nil)
+					// Pull out ExpiresOn field,
+					// so we can assert.Equals on the rest of the struct
+					expectedLeaseCopy := expectedLease
+					expectedLeaseCopy.ExpiresOn = 0
+					actualLeaseCopy := params.Lease
+					actualLeaseCopy.ExpiresOn = 0
 
-	// Mock the Authentication service (would pop open browser to auth user)
-	authSvc := &mocks.Authenticater{}
-	authSvc.On("Authenticate").Return(nil)
+					// Check the rest of the lease object
+					assert.Equal(t, expectedLeaseCopy, actualLeaseCopy)
+					return true
+				}), nil).
+				Return(func(params *operations.PostLeasesParams, wt runtime.ClientAuthInfoWriter) *operations.PostLeasesCreated {
+					// Return a Lease, which looks like the requested lease
+					// (but with an account ID)
+					return &operations.PostLeasesCreated{
+						Payload: &operations.PostLeasesCreatedBody{
+							AccountID:                testCase.mockAccountID,
+							BudgetAmount:             *params.Lease.BudgetAmount,
+							BudgetCurrency:           *params.Lease.BudgetCurrency,
+							BudgetNotificationEmails: params.Lease.BudgetNotificationEmails,
+							ExpiresOn:                params.Lease.ExpiresOn,
+							PrincipalID:              *params.Lease.PrincipalID,
+						},
+					}
+				}, nil)
 
-	cli.Inject(func(input *injectorInput) {
-		service.ApiClient = api
-		service.Out = out
-		input.service.Authenticater = authSvc
-	})
+			// Mock the output writer
+			out := &mocks.OutputWriter{}
+			out.On("Write", mock.MatchedBy(func(out []byte) bool {
+				var actualJSONOutput map[string]interface{}
+				err := json.Unmarshal(out, &actualJSONOutput)
+				assert.Nil(t, err, "output should be valid JSON")
 
-	// Run `dce leases create` command
-	err := cli.Execute(testCase.commandArgs)
-	require.Nil(t, err)
+				// Check that ExpiresOn is within ~5s of our expectation
+				// to account for actual time passing
+				_ = testCase.cliOutputs.printedJson
+				expectedExpiresOn := testCase.cliOutputs.printedJson["expiresOn"].(int64)
+				actualExpiresOn := actualJSONOutput["expiresOn"].(float64)
+				assert.InDelta(t,
+					expectedExpiresOn,
+					actualExpiresOn,
+					5,
+				)
 
-	api.AssertExpectations(t)
-	out.AssertNumberOfCalls(t, "Write", 1)
+				// Pull out the expiresOn field,
+				// so we can assert.Equals on the rest of JSON
+				actualJSONOutput["expiresOn"] = int64(0)
+				testCase.cliOutputs.printedJson["expiresOn"] = int64(0)
+
+				assert.Equal(t, testCase.cliOutputs.printedJson, actualJSONOutput)
+
+				return true
+			})).Return(0, nil)
+
+			// Mock the Authentication service (would pop open browser to auth user)
+			authSvc := &mocks.Authenticater{}
+			authSvc.On("Authenticate").Return(nil)
+
+			cli.Inject(func(input *injectorInput) {
+				service.ApiClient = api
+				service.Out = out
+				input.service.Authenticater = authSvc
+			})
+
+			// Run `dce leases create` command
+			err := cli.Execute(testCase.cliInputs)
+			require.Nil(t, err)
+
+			api.AssertExpectations(t)
+			out.AssertNumberOfCalls(t, "Write", 1)
+		})
+	}
 }
