97628fef8c524c2ba992b6ad013d6de858b046bc
Joshua Marsh <joshmrsh@amazon.com>
2020-04-23 13:36:21 -0500

    added nolint annotation

--- internal/util/prompt.go
+++ internal/util/prompt.go
@@ -2,10 +2,9 @@ package util
 
 import (
 	"fmt"
-	"github.com/chzyer/readline"
 	"github.com/Optum/dce-cli/configs"
 	observ "github.com/Optum/dce-cli/internal/observation"
-
+	"github.com/chzyer/readline"
 )
 
 type PromptUtil struct {
@@ -18,7 +17,7 @@ func (u *PromptUtil) PromptBasic(label string, validator func(input string) erro
 		Prompt:                 fmt.Sprint(label, " "),
 		DisableAutoSaveHistory: true,
 	})
-	defer rl.Close()
+	defer rl.Close() //nolint,errcheck
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -27,7 +26,7 @@ func (u *PromptUtil) PromptBasic(label string, validator func(input string) erro
 	if err != nil {
 		log.Fatalln(err)
 	}
-	if validator != nil{
+	if validator != nil {
 		err = validator(input)
 		if err != nil {
 			log.Fatalln(err)
@@ -36,4 +35,3 @@ func (u *PromptUtil) PromptBasic(label string, validator func(input string) erro
 
 	return &input
 }
-
