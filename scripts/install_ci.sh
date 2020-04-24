2da2f43dbffbffb41b869803c585a94f48b2f132
Joshua Marsh <joshmrsh@amazon.com>
2020-04-24 07:35:21 -0500

    trying previous golangci-lint version

--- scripts/install_ci.sh
+++ scripts/install_ci.sh
@@ -9,5 +9,5 @@ go get github.com/jstemmer/go-junit-report
 go get github.com/axw/gocov/gocov
 go get github.com/AlekSi/gocov-xml
 go get github.com/matm/gocov-html
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint@v1.24.0
 go get -u github.com/securego/gosec/cmd/gosec
